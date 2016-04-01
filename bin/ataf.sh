be5347595cec88698cc947de9c97a2d3524a55a2
Michele Venturi <dardo82@gmail.com>
2016-04-01 16:19:56 +0200

    Update ataf.sh
    Sleep more... :-P

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -2,4 +2,4 @@
 BIN=${0/.\//$PWD\/}; DIR=${BIN%/*}; NAME=${(L)${DIR%/*}##*/}; LCD="../txt/$NAME.lcd"; DEV=$(echo /dev/cu.usbmodem*)
 cd $DIR;./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &; SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV &; awk END{print} $LCD > $DEV
 awk "BEGIN{slnr=$SLNR;dev=\"$DEV\"}"'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | zsh
-echo -n -e "\xFE\x01" > $DEV; kill %%; kill %%
+echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 1; kill %%
