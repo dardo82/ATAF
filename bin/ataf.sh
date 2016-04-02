7537a25f12bf21f3f7d21bf0f7c0df4fca7d2133
Michele Venturi <dardo82@gmail.com>
2016-04-03 00:57:50 +0200

    Update ataf.sh
    Fix spacing.

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -3,7 +3,7 @@
 function _ataf_lcd () {
 ./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &
 SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; awk END{print} $LCD > $DEV
-awk "BEGIN{slnr=$SLNR;dev=\"$DEV\"}"'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | sh
+awk "BEGIN{slnr=$SLNR; dev=\"$DEV\"}" 'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | sh
 echo -n -e "\xFE\x01" > $DEV; kill %%; kill %% }
 
 LINK=$(readlink $0); BIN=${LINK:-${0/.\//$PWD\/}}; NAME=${${BIN##/*/}%.*}
