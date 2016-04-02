6c81e52104864fbe20e44839cfd29899677f82dd
Michele Venturi <dardo82@gmail.com>
2016-04-03 00:21:44 +0200

    Update ataf.sh
    Fix a variable.

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -6,5 +6,5 @@ SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; awk END{print} $LCD >
 awk "BEGIN{slnr=$SLNR;dev=\"$DEV\"}"'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | sh
 echo -n -e "\xFE\x01" > $DEV; kill %%; kill %% }
 
-BIN=${0/.\//$PWD\/}; DIR=${BIN%/*}; NAME=${(L)${DIR%/*}##*/}; LCD="../txt/$NAME.lcd"; DEV=$(echo /dev/cu.usbmodem*); cd $DIR
+BIN=${0/.\//$PWD\/}; NAME=${BIN##/*/}; DIR=${BIN%/*}; LCD="../txt/$NAME.lcd"; DEV=$(echo /dev/cu.usbmodem*); cd $DIR
 while true; do _ataf_lcd $1; done
