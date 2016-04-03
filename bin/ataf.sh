05d23b23894a4d5b55d75503795937255bac413c
Michele Venturi <dardo82@gmail.com>
2016-04-03 18:54:39 +0200

    Update ataf.sh
    scroll words

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -2,9 +2,13 @@
 
 function _ataf_lcd () {
 ./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &;
+
 SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; sleep 2; awk END{print} $LCD > $DEV;
-awk "BEGIN{slnr=$SLNR; dev=\"$DEV\"}"\ 'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | zsh;
-echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 1; kill %%; }
+
+awk -F\\t '/'"'"'$/{printf "%s\t%.8s\t%s\n", $1, $2, $3; for (i=1; i<=1+length($2)-8; i++) printf "%s\n", substr($2,i,8)}' $LCD \
+| expand -t 4,13 | awk '{if ($0~/'"'"'$/) printf "\xFE\xC0%s", $0; else printf "\xFE\xC4%s", $0; system("sleep 2")}' > $DEV;
+
+echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 2; kill %%; }
 
 LINK=$(readlink $0); BIN=${LINK:-${0/.\//$PWD\/}}; NAME=${${BIN##/*/}%.*}
 DIR=${BIN%/*}; LCD="../txt/$NAME.lcd"; DEV=(/dev/cu.usbmodem*)
