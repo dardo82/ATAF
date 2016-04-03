29189214b08378c8239992f8be93f2e4fbba676f
Michele Venturi <dardo82@gmail.com>
2016-04-03 08:35:19 +0200

    Update ataf.sh
    sleep once

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -2,7 +2,7 @@
 
 function _ataf_lcd () {
 ./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &;
-SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; awk END{print} $LCD > $DEV;
+SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; sleep 1; awk END{print} $LCD > $DEV;
 awk "BEGIN{slnr=$SLNR; dev=\"$DEV\"}"\ 'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | zsh;
 echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 1; kill %%; }
 
