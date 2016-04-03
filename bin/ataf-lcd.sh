152c652d09110dba639cb6f135429325d8a5d2a0
Michele Venturi <dardo82@gmail.com>
2016-04-03 22:45:07 +0200

    Update ataf-lcd.sh

--- bin/ataf-lcd.sh
+++ bin/ataf-lcd.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
-s=$(echo -n $(($(date '+%s')-$(date '+%s')%$(($((60*60))/4))))r3x1m_ataf \
+s=$(echo -en $(($(date '+%s')-$(date '+%s')%((60*60)/4)))r3x1m_ataf \
 | openssl sha1); DATE=$(date '+%a %d/%m  %R')
 
-ATAF="http://www.temporealeataf.it/Mixer/Rest/PublicTransportService.svc\
+ATAF="http://temporealeataf.it/Mixer/Rest/PublicTransportService.svc\
 /schedule?nodeID=$1&lat=43.8&lon=11.2&s=$s"
 
-BIN=${0:A}; DIR=${BIN%/*}; NAME=${${BIN##/*/}%.*}; LCD=$NAME.lcd; JSON=$NAME.json
+BIN=${0:A}; DIR=${BIN%/*}; NAME=${${BIN##/*/}%%(-*)#.[^/]##}; LCD=$NAME.lcd; JSON=$NAME.json
 
-cd $DIR/../txt; curl $ATAF > $JSON; MIN=$(date -u '+%R' | awk -F: '{print (($1*60)+$2)}')
+cd $DIR/../txt; curl -s $ATAF > $JSON; MIN=$(date -u '+%R' | awk -F: '{print (($1*60)+$2)}')
 
 awk 'BEGIN{FS="\""; RS="{|}"; min='$MIN'}{if (l!~$12) printf "%s\t%s\t%2d'"'"'\n"\
 , $12, $20, ((($4/1000)/60)-min); l=l$12}' $JSON > $LCD
 
-echo -e \\n${(U)DATE} >> $LCD
+echo -en \\n${(U)DATE} >> $LCD
