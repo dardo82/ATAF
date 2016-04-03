5993c595b2488a5f70e415b0ab88e95fe0e19c55
Michele Venturi <dardo82@gmail.com>
2016-04-03 23:12:05 +0200

    Update ataf-lcd.sh

--- bin/ataf-lcd.sh
+++ bin/ataf-lcd.sh
@@ -6,7 +6,7 @@ s=$(echo -en $(($(date '+%s')-$(date '+%s')%((60*60)/4)))r3x1m_ataf \
 ATAF="http://temporealeataf.it/Mixer/Rest/PublicTransportService.svc\
 /schedule?nodeID=$1&lat=43.8&lon=11.2&s=$s"
 
-BIN=${0:A}; DIR=${BIN%/*}; NAME=${${BIN##/*/}%%(-*)#.[^/]##}; LCD=$NAME.lcd; JSON=$NAME.json
+BIN=${0:A}; DIR=${BIN%/*}; NAME=${${${BIN##/*/}%.*}%%-*}; LCD="$NAME.lcd"; JSON="$NAME.json"
 
 cd $DIR/../txt; curl -s $ATAF > $JSON; MIN=$(date -u '+%R' | awk -F: '{print (($1*60)+$2)}')
 
