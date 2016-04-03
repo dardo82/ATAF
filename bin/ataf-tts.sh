4ac1d45b8467e2ed70a6fbeb008cd1592cd6a2a0
Michele Venturi <dardo82@gmail.com>
2016-04-03 20:34:19 +0200

    Update ataf-tts.sh
    read better

--- bin/ataf-tts.sh
+++ bin/ataf-tts.sh
@@ -1,8 +1,16 @@
 #!/bin/zsh
 
-LINK=$(readlink $0); DIR=${${LINK:-$0}%/*}; cd $DIR/../txt; awk '/:/{print "Sono le ore "$3"."}' ataf.lcd > ataf.tts
-awk -F"'|\t" /\'/'{print "Linea "$1" per "$2" in arrivo fra "$3" minuti."}' ataf.lcd >> ataf.tts
+BIN=${0:A}; DIR=${BIN%/*}; NAME=${${BIN##/*/}%.*}; LCD=$NAME.lcd; TTS=$NAME.tts
 
-CRED="a3aa517e-0b36-4095-bdfa-3fc85370ba0f:bwoEuQ14QCJG"; ACCEPT="audio/flac"; VOICE="it-IT_FrancescaVoice"
-TTS="https://$CRED\@stream.watsonplatform.net/text-to-speech/api/v1/synthesize\?accept=$ACCEPT\&voice=$VOICE\&text="
-curl -s $(sed "s/ /%20/g;s#^#$TTS#" ataf.tts) | play -q -t flac -
+cd $DIR/../txt; awk '/:/{print "Sono le ore "$3"."}' $LCD > $TTS
+
+awk -F"'|\t" /\'/'{print "Linea "$1" per "$2" \
+in arrivo fra "$3" minuti."}' $LCD >> $TTS
+
+CRED="a3aa517e-0b36-4095-bdfa-3fc85370ba0f:bwoEuQ14QCJG"
+ACCEPT="audio/flac"; VOICE="it-IT_FrancescaVoice"
+
+IBM="https://$CRED@stream.watsonplatform.net/text-to-speech\
+/api/v1/synthesize?accept=$ACCEPT\&voice=$VOICE\&text="
+
+curl $(sed "s/ /%20/g;s#^#$IBM#" $TTS) | play -q -t flac -
