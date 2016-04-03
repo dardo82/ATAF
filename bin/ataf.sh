bf3a7516828c46af1d5bdcd1f27e127e5e52232a
Michele Venturi <dardo82@gmail.com>
2016-04-03 21:29:55 +0200

    Update ataf.sh
    indent

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -2,15 +2,15 @@
 
 function _ataf_lcd () {
 
-./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &;
+	./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &
 
-SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; sleep 2; awk END{print} $LCD > $DEV;
+	SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; sleep 2; awk END{print} $LCD > $DEV
 
-awk -F\\t '/'"'"'$/{printf "%s\t%.8s\t%s\n", $1, $2, $3; for (i=1; i<=1+length($2)-8; i++) printf "%s\n", substr($2,i,8)}' $LCD \
-| expand -t 4,13 | awk '{if ($0~/'"'"'$/) printf "\xFE\xC0%s", $0; else printf "\xFE\xC4%s", $0; system("sleep 2")}' > $DEV;
+	awk -F\\t '/'"'"'$/{printf "%s\t%.8s\t%s\n", $1, $2, $3; for (i=1; i<=1+length($2)-8; i++) printf "%s\n", substr($2,i,8)}' $LCD \
+	| expand -t 4,13 | awk '{if ($0~/'"'"'$/) printf "\xFE\xC0%s", $0; else printf "\xFE\xC4%s", $0; system("sleep 2")}' > $DEV
 
-echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 2; kill %%; }
+	echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 2; kill %% }
 
 BIN=${0:A}; DIR=${BIN%/*}; NAME=${${BIN##/*/}%.*}; LCD="../txt/$NAME.lcd"; DEV=(/dev/cu.usbmodem*)
 
-cd $DIR; while true; do _ataf_lcd $1; done
+cd $DIR; while true; do _ataf_lcd $1; done &
