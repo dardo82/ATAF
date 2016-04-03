a648412821d25096a8578e62846daef8d5a8e1c0
Michele Venturi <dardo82@gmail.com>
2016-04-03 08:26:25 +0200

    Update ataf.sh
    sleep less

--- bin/ataf.sh
+++ bin/ataf.sh
@@ -4,7 +4,7 @@ function _ataf_lcd () {
 ./$NAME-lcd.sh $1; while true; do grep -q 1 $DEV && ./$NAME-tts.sh $1; done &;
 SLNR=$(awk END{print\ NR-1} $LCD); cat $DEV > /dev/null &; awk END{print} $LCD > $DEV;
 awk "BEGIN{slnr=$SLNR; dev=\"$DEV\"}"\ 'NR<slnr{print "echo -n -e \"\\xFE\\xC0"$0"\" > "dev"; sleep "60/(slnr-1)";"}' $LCD | sh;
-sleep 60; echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 1; kill %%; }
+echo -n -e "\xFE\x01" > $DEV; kill %%; sleep 1; kill %%; }
 
 LINK=$(readlink $0); BIN=${LINK:-${0/.\//$PWD\/}}; NAME=${${BIN##/*/}%.*}
 DIR=${BIN%/*}; LCD="../txt/$NAME.lcd"; DEV=(/dev/cu.usbmodem*)
